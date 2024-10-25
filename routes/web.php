eaf22daef706e7aebff7739a27a2666fe771ad93
Fatahul Ahmad Dzikri <fatahuldzikri@gmail.com>
2024-10-25 17:03:21 +0700

    delete storage link route

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Presensi;
 use Illuminate\Support\Facades\Artisan;
 
-// Storage Link
-Route::get('/storage-link', function () {
-    Artisan::call('storage:link');
-    return 'Storage linked successfully.';
-});
-
 Route::group(['middleware' => 'auth'], function () {
     Route::get('presensi', Presensi::class)->name('presensi');
 });

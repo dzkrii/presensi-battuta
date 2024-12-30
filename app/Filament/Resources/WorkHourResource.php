1f6ef314485ee75157f90298939b5072995c5d71
Fatahul Ahmad Dzikri <fatahuldzikri@gmail.com>
2024-12-30 13:10:37 +0700

    Mengubah WorkHourResource

--- app/Filament/Resources/WorkHourResource.php
+++ app/Filament/Resources/WorkHourResource.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\WorkHourResource\Pages;
 use App\Filament\Resources\WorkHourResource\RelationManagers;
+use App\Models\User;
 use App\Models\WorkHour;
+use Carbon\Carbon;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -13,10 +15,11 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
 class WorkHourResource extends Resource
 {
-    protected static ?string $model = WorkHour::class;
+    protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-clock';
 
@@ -30,15 +33,15 @@ class WorkHourResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('user_id')
-                    ->relationship('user', 'name')
-                    ->searchable()
-                    ->preload()
-                    ->required(),
-                Forms\Components\TextInput::make('total_hours')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
+                // Forms\Components\Select::make('user_id')
+                //     ->relationship('user', 'name')
+                //     ->searchable()
+                //     ->preload()
+                //     ->required(),
+                // Forms\Components\TextInput::make('total_hours')
+                //     ->required()
+                //     ->numeric()
+                //     ->default(0),
             ]);
     }
 
@@ -48,44 +51,111 @@ class WorkHourResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user.name')
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('total_hours')
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('work_duration')
-                    ->label('Durasi Kerja')
-                    ->getStateUsing(function ($record) {
-                        // Ambil nilai bulan dan tahun dari request
-                        $month = request()->input('filters.month', date('n')); // default bulan sekarang
-                        $year = request()->input('filters.year', date('Y'));   // default tahun sekarang
+                // Tables\Columns\TextColumn::make('user.name')
+                //     ->sortable(),
+                // Tables\Columns\TextColumn::make('total_hours')
+                //     ->sortable(),
+                // Tables\Columns\TextColumn::make('work_duration')
+                //     ->label('Durasi Kerja')
+                //     ->getStateUsing(function ($record) {
+                //         // Ambil nilai bulan dan tahun dari request
+                //         $month = request()->input('filters.month', date('n')); // default bulan sekarang
+                //         $year = request()->input('filters.year', date('Y'));   // default tahun sekarang
 
-                        return $record->getMonthlyWorkDuration($month, $year);
-                    }),
-                Tables\Columns\TextColumn::make('missing_work_hours')
-                    ->label('Kekurangan Jam Kerja')
-                    ->getStateUsing(function ($record) {
-                        // Ambil filter bulan dan tahun dari query
-                        $month = request('tableFilters')['month'] ?? date('n'); // Default bulan sekarang
-                        $year = request('tableFilters')['year'] ?? date('Y');  // Default tahun sekarang
+                //         return $record->getMonthlyWorkDuration($month, $year);
+                //     }),
+                // Tables\Columns\TextColumn::make('missing_work_hours')
+                //     ->label('Kekurangan Jam Kerja')
+                //     ->getStateUsing(function ($record) {
+                //         // Ambil filter bulan dan tahun dari query
+                //         $month = request('tableFilters')['month'] ?? date('n'); // Default bulan sekarang
+                //         $year = request('tableFilters')['year'] ?? date('Y');  // Default tahun sekarang
 
-                        // Panggil fungsi untuk menghitung kekurangan jam kerja
-                        return $record->getMissingWorkHours($month, $year);
+                //         // Panggil fungsi untuk menghitung kekurangan jam kerja
+                //         return $record->getMissingWorkHours($month, $year);
+                //     }),
+                // Tables\Columns\TextColumn::make('created_at')
+                //     ->dateTime()
+                //     ->sortable()
+                //     ->toggleable(isToggledHiddenByDefault: true),
+                // Tables\Columns\TextColumn::make('updated_at')
+                //     ->dateTime()
+                //     ->sortable()
+                //     ->toggleable(isToggledHiddenByDefault: true),
+                // Tables\Columns\TextColumn::make('deleted_at')
+                //     ->dateTime()
+                //     ->sortable()
+                //     ->toggleable(isToggledHiddenByDefault: true),
+
+                // ! BARU
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama Karyawan')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('current_work_hours')
+                    ->label('Total Jam Kerja')
+                    ->getStateUsing(function ($record) {
+                        // Tentukan rentang tanggal
+                        $startDate = Carbon::create(2024, 12, 2)->startOfDay();
+                        $endDate = Carbon::create(2024, 12, 7)->endOfDay();
+
+                        // Ambil data presensi dalam rentang tanggal tersebut
+                        $attendances = $record->attendances()
+                            ->whereBetween('created_at', [$startDate, $endDate])
+                            ->whereNotNull('end_time')
+                            ->get();
+
+                        $totalHours = 0;
+                        $totalMinutes = 0;
+
+                        foreach ($attendances as $attendance) {
+                            $checkIn = Carbon::parse($attendance->start_time);
+                            $checkOut = Carbon::parse($attendance->end_time);
+                            $duration = $checkIn->diff($checkOut);
+
+                            $totalHours += $duration->h;
+                            $totalMinutes += $duration->i;
+                        }
+
+                        // Konversi menit menjadi jam jika lebih dari 60 menit
+                        $totalHours += intdiv($totalMinutes, 60);
+                        $totalMinutes = $totalMinutes % 60;
+
+                        return $totalHours . ' jam ' . $totalMinutes . ' menit';
                     }),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('deleted_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                // 
+                // Tables\Filters\SelectFilter::make('month')
+                //     ->label('Bulan')
+                //     ->options([
+                //         '1' => 'Januari',
+                //         '2' => 'Februari',
+                //         '3' => 'Maret',
+                //         '4' => 'April',
+                //         '5' => 'Mei',
+                //         '6' => 'Juni',
+                //         '7' => 'Juli',
+                //         '8' => 'Agustus',
+                //         '9' => 'September',
+                //         '10' => 'Oktober',
+                //         '11' => 'November',
+                //         '12' => 'Desember',
+                //     ])
+                //     ->default(date('n')),
+                // Tables\Filters\SelectFilter::make('year')
+                //     ->label('Tahun')
+                //     ->options([
+                //         date('Y') => date('Y'),
+                //         date('Y') - 1 => date('Y') - 1,
+                //     ])
+                //     ->default(date('Y')),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -94,6 +164,7 @@ class WorkHourResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
+                ExportBulkAction::make()
             ]);
     }
 
